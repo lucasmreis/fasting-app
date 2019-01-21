8c55f24caf32b2728778c0f951a2674f7b442f6e
Lucas Reis <lucasmreis@gmail.com>
2019-01-21 16:15:35 -0500

    with activity

--- src/machine.js
+++ src/machine.js
@@ -1,50 +1,79 @@
-import { Machine } from "xstate";
+import { Machine, actions } from "xstate";
 
-const lightMachine = Machine({
-  id: "fastingScreen",
-  initial: "notFasting",
-  states: {
-    notFasting: {
-      on: { START_FAST: "fasting" }
-    },
-    fasting: {
-      onEntry: ["saveStartTime"],
-      onExit: ["saveEndTime"],
-      on: { END_FAST: "congratulations" },
-      initial: "visible",
-      states: {
-        checkVisible: {
-          on: {
-            "": [
-              {
-                target: "visible",
-                cond: c => c.isVisible
-              },
-              {
-                target: "notVisible",
-                cond: c => !c.isVisible
-              }
-            ]
-          }
-        },
-        visible: {
-          onEntry: ["startTimer"],
-          onExit: ["stopTimer"],
-          on: {
-            CHANGE_VISIBILITY: "notVisible"
-          }
-        },
-        notVisible: {
-          on: {
-            CHANGE_VISIBILITY: "visible"
+// Available variables:
+// Machine (machine factory function)
+// XState (all XState exports)
+
+const fastingMachine = Machine(
+  {
+    id: "fastingScreen",
+    initial: "notFasting",
+    context: { startTime: null },
+    states: {
+      notFasting: {
+        on: { START_FAST: "fasting" }
+      },
+      fasting: {
+        onEntry: ["saveStartTime"],
+        onExit: ["saveFast"],
+        activities: ["visibility"],
+        on: { END_FAST: "congratulations" },
+        initial: "visible",
+        states: {
+          checkVisible: {
+            on: {
+              "": [
+                {
+                  target: "visible",
+                  cond: c => c.isVisible
+                },
+                {
+                  target: "notVisible",
+                  cond: c => !c.isVisible
+                }
+              ]
+            }
+          },
+          visible: {
+            activities: ["timer"],
+            on: {
+              CHANGE_VISIBILITY: "notVisible"
+            }
+          },
+          notVisible: {
+            on: {
+              CHANGE_VISIBILITY: "visible"
+            }
           }
         }
+      },
+      congratulations: {
+        on: {
+          ACK: "notFasting"
+        }
+      }
+    }
+  },
+  {
+    actions: {
+      saveStartTime: actions.assign({ startTime: new Date() }),
+      saveFast: function(ctx) {
+        console.log(
+          "Fasting saved! Started at " +
+            ctx.startTime +
+            " and finished at " +
+            new Date()
+        );
       }
     },
-    congratulations: {
-      on: {
-        ACK: "notFasting"
+    activities: {
+      timer: function(ctx) {
+        const t = setInterval(
+          () => console.log(new Date() - ctx.startTime),
+          1000
+        );
+        return () => clearInterval(t);
       }
     }
   }
-});
+);
